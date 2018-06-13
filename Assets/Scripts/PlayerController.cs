5e6a056f83f951bbae8d6fceb6590e30b48ce9c3
Bulletdrone <Sam>
2018-06-13 10:01:49 +0200

    First Commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+	public float speed;
+
+	Rigidbody rb;
+	Vector3 direction;
+
+	void Start () {
+		speed = 1000;
+		rb = GetComponent<Rigidbody> ();
+	}
+
+	void Update () {
+		float horizontalMovement = Input.GetAxisRaw ("Horizontal");
+		float verticalMovement = Input.GetAxisRaw ("Vertical");
+
+		direction = (horizontalMovement * transform.right + verticalMovement * transform.forward).normalized;
+	}
+
+	void FixedUpdate () {
+		rb.velocity = direction * speed * Time.deltaTime;
+	}
+}
