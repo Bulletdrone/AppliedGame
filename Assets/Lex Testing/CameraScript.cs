ed49834b7b89ea603165c9e4c56927e5d1203448
Flashcaptain <36227316+Flashcaptain@users.noreply.github.com>
2018-06-14 10:15:40 +0200

    alles wat ik zever heb gedaan

---
+++ Assets/Lex Testing/CameraScript.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour {
+	public Transform target;
+	public float cameraSpeed = 15;
+	float Offset = 72.5f;
+	float yOffset = -60f;
+	public bool smoothFollow = true;
+	public bool freeLook = false;
+	public float freeCamSpeed = 0.2f;
+
+	void Update () {
+
+		Vector3 pos = transform.position;
+		if (Input.GetMouseButton(2))
+		{
+			pos.x -= Input.GetAxis("Mouse X") + Input.GetAxis("Mouse Y") * freeCamSpeed* Time.deltaTime;
+			pos.z -= Input.GetAxis("Mouse Y") - Input.GetAxis("Mouse X") * freeCamSpeed* Time.deltaTime;
+		}
+
+		transform.position = pos;
+
+
+		if (Input.GetKeyDown (KeyCode.C)) {
+			smoothFollow = false; 
+		} else {
+			smoothFollow = true;
+		}
+		if (Input.GetKey (KeyCode.Mouse2)) {
+			freeLook = true; 
+		} else {
+			freeLook = false;
+		}
+
+		if (target) {
+			Vector3 newPos = transform.position;
+			newPos.x = target.position.x - Offset;
+			newPos.y = target.position.y - yOffset;
+			newPos.z = target.position.z - Offset;
+
+
+			if (!smoothFollow) {
+				transform.position = newPos;
+			} else if (!freeLook) {
+				transform.position = Vector3.Lerp (transform.position, newPos, cameraSpeed * Time.deltaTime);
+			} else if (freeLook) {
+				;
+			}
+		}﻿
+	}
+}
---
+++ Assets/Lex Testing/PlayerMovementScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PlayerMovementScript : MonoBehaviour {
+	float Speed = 3;
+	public Transform target;
+	Rigidbody rb;
+	Transform aF;
+
+	// Use this for initialization
+	void Start () {
+		rb = gameObject.GetComponentInChildren <Rigidbody>();
+		aF = transform.GetChild (0);
+	}
+
+
+	// Update is called once per frame
+	void FixedUpdate () {
+
+		Vector3 test = new Vector3 (Input.GetAxis ("Vertical") - Input.GetAxis ("Horizontal"), 0, Input.GetAxis ("Vertical") + Input.GetAxis ("Horizontal")) * Speed;
+		rb.velocity = (new Vector3 (test.z, rb.velocity.y, test.x));
+
+		aF.localPosition = rb.velocity.normalized;
+
+	}
+
+	void OnTriggerEnter(Collider CEnter){
+		//if (CEnter.gameObject.CompareTag ("...")) {
+	
+		//if (CEnter.gameObject.name == "...") {
+	}
+	void OnTriggerExit(Collider CExit){
+		//if (CExit.gameObject.CompareTag("...")) {
+	}
+}
